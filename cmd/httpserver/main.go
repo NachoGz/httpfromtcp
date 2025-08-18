bd784e396419fb877a0f9a8367863d7c9d7a2bd5
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-08-18 00:06:09 -0300

    add main package for the http server functionality

---
+++ cmd/httpserver/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"io"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"httpfromtcp/internal/request"
+	"httpfromtcp/internal/response"
+	"httpfromtcp/internal/server"
+)
+
+const port = 42069
+
+func handlerFunc(w io.Writer, req *request.Request) *server.HandlerError {
+	switch req.RequestLine.RequestTarget {
+	case "/yourproblem":
+		return &server.HandlerError{
+			StatusCode: response.StatusBadRequest,
+			Message: "Your problem is not my problem\n",
+		}
+	case "/myproblem":
+		return &server.HandlerError{
+			StatusCode: response.StatusInternalServerError,
+			Message: "Woopsie, my bad\n",
+		}
+	default:
+		w.Write([]byte("All good, frfr\n"))
+	}
+	return &server.HandlerError{StatusCode: response.StatusOK}
+}
+
+func main() {
+	server, err := server.Serve(port, handlerFunc)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
+	defer server.Close()
+	log.Println("Server started on port", port)
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+	<-sigChan
+
+
+	log.Println("Server gracefully stopped")
+}
