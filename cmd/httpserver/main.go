ac0bf4d605cb7c04130123e61d8b1e1a05f0962f
Ignacio Gomez <ignaciogomezf1@gmail.com>
2025-08-18 22:29:13 -0300

    response with an html according to the status code. Moved all the writing of status line, headers and body to an outside function to avoid repeating in the handler

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"io"
 	"log"
 	"os"
 	"os/signal"
@@ -14,22 +13,50 @@ import (
 
 const port = 42069
 
-func handlerFunc(w io.Writer, req *request.Request) *server.HandlerError {
+func formatResponse(statusCode response.StatusCode) string {
+	body := "<html>\n\t<head>\n"
+	body += "\t</head>\n<body>"
+	switch statusCode {
+	case response.StatusOK:
+		body += "\t<title>200 OK</title>\n"
+		body += "\t\t<h1>Success!</h1>\n"
+		body += "\t\t<p>Your request was an absolute banger.</p>\n"
+	case response.StatusBadRequest:
+		body += "\t<title>400 Bad Request</title>\n"
+		body += "\t\t<h1>Bad Request</h1>\n"
+		body += "\t\t<p>Your request honestly kinda sucked.</p>\n"
+	case response.StatusInternalServerError:
+		body += "\t<title>500 Internal Server Error</title>\n"
+		body += "\t\t<h1>Internal Server Error</h1>\n"
+		body += "\t\t<p>Okay, you know what? This one is on me.</p>\n"
+	}
+
+	body += "\t</body>\n"
+	body += "</html>\n"
+	return body
+}
+
+func writeResponse(w *response.Writer, statusCode response.StatusCode) {
+	body := formatResponse(statusCode)
+
+	// write status line and headers to the connection
+	w.WriteStatusLine(statusCode)
+	h := response.GetDefaultHeaders(len(body))
+	h.Set("Content-Type", "text/html")
+	w.WriteHeaders(h)
+	// write the response body from the handler's buffer to the connection
+	w.WriteBody([]byte(body))
+}
+
+func handlerFunc(w *response.Writer, req *request.Request)  {
 	switch req.RequestLine.RequestTarget {
 	case "/yourproblem":
-		return &server.HandlerError{
-			StatusCode: response.StatusBadRequest,
-			Message: "Your problem is not my problem\n",
-		}
+		writeResponse(w, response.StatusBadRequest)
 	case "/myproblem":
-		return &server.HandlerError{
-			StatusCode: response.StatusInternalServerError,
-			Message: "Woopsie, my bad\n",
-		}
+		writeResponse(w, response.StatusInternalServerError)
 	default:
-		w.Write([]byte("All good, frfr\n"))
+		writeResponse(w, response.StatusOK)
 	}
-	return nil
 }
 
 func main() {
